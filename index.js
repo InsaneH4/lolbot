02b536ac6ea55526e3ed4e0d919b199f123b3c4b
Samuel Hussain <samuelhussain@live.com>
2022-03-04 17:49:20 -0800

    improved meme command

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ client.on("messageCreate", function (message) {
     }
     if (command === "meme" && message.content.startsWith(prefix)) {
         let topText = message.content.substring(message.content.indexOf(' '), message.content.indexOf(','))
-        let bottomText = message.content.substring(message.content.indexOf(','))
+        let bottomText = message.content.substring(message.content.indexOf(',' + 1))
         let meme = topText + "\n\n\n\n\n" + bottomText
         message.channel.send(meme)
     }
